650c5c3d4e6a9eca6b285249b926ad32260c115b
Marcelo Morgado <cmarcelom@gmail.com>
2018-12-18 17:12:07 +0000

    refs #12

--- demo/screens/Home.js
+++ demo/screens/Home.js
@@ -1,16 +1,9 @@
 import React from "react";
 import { Image, Button, StyleSheet, View } from "react-native";
-import { Account } from "tasit-sdk";
 import LargeText from "../components/presentational/LargeText";
 import Colors from "../constants/Colors";
 
 export default class Home extends React.Component {
-  componentDidMount = async () => {
-    // TODO: Remove await when SDK 0.0.3 is out
-    const wallet = await Account.create();
-    console.log(wallet.address);
-  };
-
   render() {
     return (
       <View style={styles.container}>
