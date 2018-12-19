8416143ef4e6941de34573b9f997531baf9b1156
Paul Cowgill <pauldavidcowgill@gmail.com>
2018-12-19 06:04:31 -0500

    Update demo/components/presentational/Home.js
    Co-Authored-By: marcelomorgado <cmarcelom@gmail.com>

--- demo/components/presentational/Home.js
+++ demo/components/presentational/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Image, Button, StyleSheet, View } from "react-native";
 import LargeText from "./LargeText";
-import Colors from "../../constants/Colors";
+import Colors from "@constants/Colors";
 
 export default function Home(props) {
   return (
