bf976c40395f707aee2c2d61c1a003d7b489d0c6
Paul Cowgill <pauldavidcowgill@gmail.com>
2018-12-19 06:04:48 -0500

    Update demo/components/presentational/ListLands.js
    Co-Authored-By: marcelomorgado <cmarcelom@gmail.com>

--- demo/components/presentational/ListLands.js
+++ demo/components/presentational/ListLands.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ListView, StyleSheet } from "react-native";
-import Colors from "../../constants/Colors";
+import Colors from "@constants/Colors";
 
 export default function ListLands(props) {
   return (
