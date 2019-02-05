c719a697e5c40d74c5200b40cc309b411f0195cd
Marcelo Morgado <cmarcelom@gmail.com>
2019-02-05 20:21:54 +0000

    Home screen Decentraland button removal

--- demo/components/presentational/Home.js
+++ demo/components/presentational/Home.js
@@ -1,19 +1,13 @@
 import React from "react";
-<<<<<<< HEAD
-import { Image, StyleSheet, View, ActivityIndicator } from "react-native";
+import {
+  Image,
+  Button,
+  StyleSheet,
+  View,
+  ActivityIndicator,
+} from "react-native";
 import LargeText from "./LargeText";
 import Colors from "@constants/Colors";
-
-export default function Home() {
-  return (
-    <View style={styles.container}>
-      <Image source={require("../../assets/images/icon.png")} />
-      <LargeText>{`Tasit Decentraland`}</LargeText>
-      <ActivityIndicator size="large" color={Colors.loadingColor} />
-=======
-import { Image, Button, StyleSheet, View } from "react-native";
-import LargeText from "./LargeText";
-import Colors from "../../constants/Colors";
 
 export default function Home(props) {
   return (
@@ -21,7 +15,8 @@ export default function Home(props) {
       <Image source={require("../assets/images/icon.png")} />
       <LargeText>{`Tasit`}</LargeText>
       <Button title="Decentraland" onPress={props.onPress} />
->>>>>>> Screen/Presentational components refactoring
+      <LargeText>{`Decentraland`}</LargeText>
+      <ActivityIndicator size="large" color={Colors.loadingColor} />
     </View>
   );
 }
--- demo/screens/HomeScreen.js
+++ demo/screens/HomeScreen.js
@@ -1,31 +1,15 @@
 import React from "react";
-<<<<<<< HEAD
 import Home from "@presentational/Home";
 import { NavigationEvents } from "react-navigation";
-
-export default class HomeScreen extends React.Component {
-  waitAndNavigate() {
-    setTimeout(() => {
-      this.props.navigation.navigate("ListLandForSaleScreen");
-    }, 1500);
-  }
-
-  // This screen should shows something nice-looking
-  // if the connection to the blockchain can't be estabilished.
-  render() {
-    return (
-      <React.Fragment>
-        <NavigationEvents onDidFocus={() => this.waitAndNavigate()} />
-        <Home />
-      </React.Fragment>
-=======
-import Home from "../components/presentational/Home";
 
 export default class HomeScreen extends React.Component {
   render() {
     return (
       <Home onPress={() => this.props.navigation.navigate("ListLandsScreen")} />
->>>>>>> Screen/Presentational components refactoring
+      <React.Fragment>
+        <NavigationEvents onDidFocus={() => this.waitAndNavigate()} />
+        <Home />
+      </React.Fragment>
     );
   }
 }
