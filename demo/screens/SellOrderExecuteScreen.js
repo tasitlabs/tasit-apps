1647090407c1138b03c1eb473aed3d56576ae3b2
Marcelo Morgado <cmarcelom@gmail.com>
2019-02-12 14:22:42 +0000

    helpers file

--- demo/screens/SellOrderExecuteScreen.js
+++ demo/screens/SellOrderExecuteScreen.js
@@ -5,23 +5,23 @@ import SellOrderExecute from "@presentational/SellOrderExecute";
 import PropTypes from "prop-types";
 
 import ContractsABIs from "@constants/ContractsABIs";
-const { manaABI, estateABI, marketplaceABI } = ContractsABIs;
+const { estateABI, marketplaceABI } = ContractsABIs;
 import ContractsAddresses from "@constants/ContractsAddresses";
-const { manaAddress, estateAddress, marketplaceAddress } = ContractsAddresses;
+const { estateAddress, marketplaceAddress } = ContractsAddresses;
 
 import { Action } from "tasit-sdk";
 const { Contract } = Action;
-import { createFromPrivateKey } from "tasit-account/dist/testHelpers/helpers";
+import { approveManaSpending, manaFaucetTo } from "./helpers";
 
 export class SellOrderExecuteScreen extends React.Component {
   // TODO: Switch to new DecentralandEstate() once the SDK includes that
   estateContract = new Contract(estateAddress, estateABI);
   marketplaceContract = new Contract(marketplaceAddress, marketplaceABI);
-  manaContract = new Contract(manaAddress, manaABI);
 
   _onOrderExecution = sellOrder => {
     const { navigation, account, removeSellOrder } = this.props;
     const afterSuccessfulExecution = () => {
+      // Should rename that to remove___FromUI or is it obvious?
       removeSellOrder(sellOrder);
     };
     this._executeOrder(sellOrder, account, afterSuccessfulExecution);
@@ -47,10 +47,10 @@ export class SellOrderExecuteScreen extends React.Component {
     const marketplaceAddress = this.marketplaceContract.getAddress();
     const estateAddress = this.estateContract.getAddress();
 
-    await this._manaFaucetTo(account, TEN);
+    await manaFaucetTo(account, TEN);
 
     // TODO: Move this out to an explicit approval flow?
-    await this._approveManaSpending(account, marketplaceAddress, ONE);
+    await approveManaSpending(account, marketplaceAddress, ONE);
 
     this.marketplaceContract.setWallet(account);
 
@@ -69,29 +69,6 @@ export class SellOrderExecuteScreen extends React.Component {
     // TODO: This function should be called inside of the eventListener
     // that catches the safeExecuteOrder successful event.
     afterSuccessfulExecution();
-  };
-
-  _approveManaSpending = async (fromAccount, toAddress, value) => {
-    this.manaContract.setWallet(fromAccount);
-    const approvalAction = this.manaContract.approve(
-      toAddress,
-      value.toString()
-    );
-
-    await approvalAction.waitForNonceToUpdate();
-  };
-
-  _manaFaucetTo = async (beneficiary, amountInWei) => {
-    const ownerPrivKey =
-      "0x11d943d7649fbdeb146dc57bd9cfc80b086bfab2330c7b25651dbaf382392f60";
-    const ownerWallet = createFromPrivateKey(ownerPrivKey);
-
-    this.manaContract.setWallet(ownerWallet);
-    const mintManaToBuyer = this.manaContract.mint(
-      beneficiary.address,
-      amountInWei.toString()
-    );
-    await mintManaToBuyer.waitForNonceToUpdate();
   };
 
   render() {
