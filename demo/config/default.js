324e7b41eb45585ceb66a084956f7138e2146bde
Marcelo Morgado <cmarcelom@gmail.com>
2019-02-17 16:09:17 +0000

    Reverting config changes.

--- demo/config/default.js
+++ demo/config/default.js
@@ -4,7 +4,7 @@ module.exports = {
     provider: "jsonrpc",
     pollingInterval: 50,
     jsonRpc: {
-      url: "http://192.168.1.70",
+      url: "http://192.168.1.69",
       port: 8545,
     },
   },
