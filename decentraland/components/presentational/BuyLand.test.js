29bac214388c1f957d61e465aadc5588f7ec8b7e
Marcelo Morgado <cmarcelom@gmail.com>
2019-03-29 17:03:08 +0000

    Test suite fix

--- decentraland/components/presentational/BuyLand.test.js
+++ decentraland/components/presentational/BuyLand.test.js
@@ -9,7 +9,27 @@ describe("BuyLand", () => {
 
     it("initial state - before start account setup", async () => {
       const accountInfo = {
+        account: null,
         setupInProgress: false,
+        fundedWithEthers: false,
+        fundedWithMana: false,
+        approvedMarketplace: false,
+      };
+      expect(
+        shallow(
+          <BuyLand
+            landForSale={parcelForSale}
+            onBuy={onBuy}
+            accountInfo={accountInfo}
+          />
+        )
+      ).toMatchSnapshot();
+    });
+
+    it("waiting for setup - waiting for account generation", async () => {
+      const accountInfo = {
+        account: null,
+        setupInProgress: true,
         fundedWithEthers: false,
         fundedWithMana: false,
         approvedMarketplace: false,
@@ -27,6 +47,7 @@ describe("BuyLand", () => {
 
     it("waiting for setup - waiting for ETH funding", async () => {
       const accountInfo = {
+        account: {},
         setupInProgress: true,
         fundedWithEthers: false,
         fundedWithMana: false,
@@ -45,6 +66,7 @@ describe("BuyLand", () => {
 
     it("waiting for setup - waiting for MANA funding and marketplace approval", async () => {
       const accountInfo = {
+        account: {},
         setupInProgress: true,
         fundedWithEthers: true,
         fundedWithMana: false,
@@ -63,6 +85,7 @@ describe("BuyLand", () => {
 
     it("waiting for setup - waiting for MANA funding", async () => {
       const accountInfo = {
+        account: {},
         setupInProgress: true,
         fundedWithEthers: true,
         fundedWithMana: false,
@@ -81,6 +104,7 @@ describe("BuyLand", () => {
 
     it("waiting for setup - waiting for marketplace approval", async () => {
       const accountInfo = {
+        account: {},
         setupInProgress: true,
         fundedWithEthers: true,
         fundedWithMana: true,
@@ -99,6 +123,7 @@ describe("BuyLand", () => {
 
     it("final state - account ready", async () => {
       const accountInfo = {
+        account: {},
         setupInProgress: false,
         fundedWithEthers: true,
         fundedWithMana: true,
