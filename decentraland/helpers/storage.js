46446a2fc35144ca8e39ac963a5951d56aa2f0b6
Marcelo Morgado <cmarcelom@gmail.com>
2019-04-04 17:00:18 +0100

    Review request

--- decentraland/helpers/storage.js
+++ decentraland/helpers/storage.js
@@ -1,4 +1,5 @@
 import { SecureStore } from "expo";
+import { AsyncStorage } from "react-native";
 import { createFromPrivateKey } from "tasit-account/dist/testHelpers/helpers";
 
 // Storage keys
@@ -7,47 +8,73 @@ const MY_ASSETS_LIST = "MY_ASSETS_LIST";
 
 export const storeEphemeralAccount = async account => {
   const { privateKey } = account;
-  await _storeData(EPHEMERAL_ACCOUNT_PRIV_KEY, privateKey);
+  await _storeData(EPHEMERAL_ACCOUNT_PRIV_KEY, privateKey, true);
 };
 
 export const retrieveEphemeralAccount = async () => {
   let account = null;
 
-  const privateKey = await _retrieveData(EPHEMERAL_ACCOUNT_PRIV_KEY);
+  const privateKey = await _retrieveData(EPHEMERAL_ACCOUNT_PRIV_KEY, true);
   if (privateKey != null) account = createFromPrivateKey(privateKey);
 
   return account;
 };
 
 export const storeMyAssets = async myAssets => {
-  const strMyAssets = JSON.stringify(myAssets);
-  await _storeData(MY_ASSETS_LIST, strMyAssets);
+  const strMyAssets = _fromJson(myAssets);
+  await _storeData(MY_ASSETS_LIST, strMyAssets, false);
 };
 
 export const retrieveMyAssets = async () => {
-  const strMyAssets = await _retrieveData(MY_ASSETS_LIST);
-  const myAssets = JSON.parse(strMyAssets);
+  const strMyAssets = await _retrieveData(MY_ASSETS_LIST, false);
+  const myAssets = _toJson(strMyAssets);
   return myAssets;
 };
 
-// Note: the value should be a string
-const _storeData = async (key, value) => {
+const _fromJson = json => {
   try {
-    // More about options:
-    // https://docs.expo.io/versions/latest/sdk/securestore/#securestoresetitemasynckey-value-options
-    const options = { keychainAccessible: SecureStore.WHEN_UNLOCKED };
-    await SecureStore.setItemAsync(key, value, options);
-  } catch (error) {
-    throw Error("Unable to securely store data.");
+    return JSON.stringify(json);
+  } catch {
+    throw Error(`Unable to parse JSON object to a String.`);
   }
 };
 
-const _retrieveData = async key => {
+const _toJson = string => {
   try {
-    const value = await SecureStore.getItemAsync(key);
+    return JSON.parse(string);
+  } catch {
+    throw Error(`Unable to parse String to a JSON object.`);
+  }
+};
+
+// Note: the value should be a string
+const _storeData = async (key, value, securely) => {
+  try {
+    if (securely) {
+      // More about options:
+      // https://docs.expo.io/versions/latest/sdk/securestore/#securestoresetitemasynckey-value-options
+      const options = { keychainAccessible: SecureStore.WHEN_UNLOCKED };
+      await SecureStore.setItemAsync(key, value, options);
+    } else {
+      await AsyncStorage.setItem(key, value);
+    }
+  } catch (error) {
+    throw Error(`Unable to ${securely ? "securely" : ""} store data.`);
+  }
+};
+
+const _retrieveData = async (key, securely) => {
+  try {
+    let value = null;
+
+    if (securely) value = await SecureStore.getItemAsync(key);
+    else value = await AsyncStorage.getItem(key);
+
     return value;
   } catch (error) {
-    throw Error("Unable to retrieve data from storage.");
+    throw Error(
+      `Unable to ${securely ? "securely" : ""} retrieve data from storage.`
+    );
   }
 };
 
