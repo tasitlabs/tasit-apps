765b0e8010aac9b56a5b0cc5290bff1c61e0e29f
Marcelo Morgado <cmarcelom@gmail.com>
2019-04-08 11:45:20 +0100

    Review request

--- decentraland/components/presentational/AccountCreationProgress.test.js
+++ decentraland/components/presentational/AccountCreationProgress.test.js
@@ -1,14 +1,106 @@
 import React from "react";
 import { shallow } from "enzyme";
 import AccountCreationProgress from "./AccountCreationProgress";
-import AccountCreationStatus from "@constants/AccountCreationStatus";
-const { FUNDING_WITH_ETH } = AccountCreationStatus;
+import status from "@constants/AccountCreationStatus";
+const {
+  NOT_STARTED,
+  GENERATING_ACCOUNT,
+  FUNDING_WITH_ETH,
+  FUNDING_WITH_MANA_AND_APPROVING_MARKETPLACE,
+  FUNDING_WITH_MANA,
+  APPROVING_MARKETPLACE,
+  READY_TO_USE,
+} = status;
 import { anAction } from "@helpers/testHelpers";
 
 describe("AccountCreationProgress", () => {
-  it("renders the component", async () => {
+  it("initial state - before start account setup", async () => {
+    const status = NOT_STARTED;
+    const currentAction = null;
+
+    expect(
+      shallow(
+        <AccountCreationProgress
+          status={status}
+          currentAction={currentAction}
+        />
+      )
+    ).toMatchSnapshot();
+  });
+
+  it("waiting for setup - waiting for account generation", async () => {
+    const status = GENERATING_ACCOUNT;
+    const currentAction = null;
+
+    expect(
+      shallow(
+        <AccountCreationProgress
+          status={status}
+          currentAction={currentAction}
+        />
+      )
+    ).toMatchSnapshot();
+  });
+
+  it("waiting for setup - waiting for ETH funding", async () => {
     const status = FUNDING_WITH_ETH;
     const currentAction = anAction;
+
+    expect(
+      shallow(
+        <AccountCreationProgress
+          status={status}
+          currentAction={currentAction}
+        />
+      )
+    ).toMatchSnapshot();
+  });
+
+  it("waiting for setup - waiting for MANA funding and marketplace approval", async () => {
+    const status = FUNDING_WITH_MANA_AND_APPROVING_MARKETPLACE;
+    const currentAction = anAction;
+
+    expect(
+      shallow(
+        <AccountCreationProgress
+          status={status}
+          currentAction={currentAction}
+        />
+      )
+    ).toMatchSnapshot();
+  });
+
+  it("waiting for setup - waiting for MANA funding", async () => {
+    const status = FUNDING_WITH_MANA;
+    const currentAction = anAction;
+
+    expect(
+      shallow(
+        <AccountCreationProgress
+          status={status}
+          currentAction={currentAction}
+        />
+      )
+    ).toMatchSnapshot();
+  });
+
+  it("waiting for setup - waiting for marketplace approval", async () => {
+    const status = APPROVING_MARKETPLACE;
+    const currentAction = anAction;
+
+    expect(
+      shallow(
+        <AccountCreationProgress
+          status={status}
+          currentAction={currentAction}
+        />
+      )
+    ).toMatchSnapshot();
+  });
+
+  it("final state - account ready", async () => {
+    const status = READY_TO_USE;
+    const currentAction = null;
 
     expect(
       shallow(
