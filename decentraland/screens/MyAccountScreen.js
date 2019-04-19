95b12566619ab22d5e83a2e0aba10a44b3efeac3
cmarcelo <cmarcelom@gmail.com>
2019-04-19 17:13:24 +0100

    Refactoring

--- decentraland/screens/MyAccountScreen.js
+++ decentraland/screens/MyAccountScreen.js
@@ -12,6 +12,11 @@ import PropTypes from "prop-types";
 
 const creationSteps = [
   {
+    name: "Account created",
+    creationStatus: GENERATING_ACCOUNT,
+    percentage: 0.25,
+  },
+  {
     creationStatus: FUNDING_WITH_ETH,
     name: "Funded with ETH",
     percentage: 0.25,
@@ -28,32 +33,36 @@ const creationSteps = [
   },
 ];
 
+const stepWasDone = (step, accountInfo) => {
+  const { creationActions, account } = accountInfo;
+  const { creationStatus } = step;
+
+  const statusWithAction = [
+    FUNDING_WITH_ETH,
+    FUNDING_WITH_MANA,
+    APPROVING_MARKETPLACE,
+  ];
+
+  if (creationStatus === GENERATING_ACCOUNT) {
+    const isAccountCreated = account !== null;
+    return isAccountCreated;
+  } else if (statusWithAction.includes(creationStatus)) {
+    const hasAnAction = creationActions[creationStatus];
+    return hasAnAction;
+  }
+};
+
 export class MyAccountScreen extends React.Component {
   render() {
     const { accountInfo } = this.props;
-    const { creationActions, account } = accountInfo;
-    const isAccountCreated = account !== null;
 
-    const accountStep = {
-      name: "Account created",
-      creationStatus: GENERATING_ACCOUNT,
-      status: isAccountCreated ? DONE : MISSING,
-      percentage: 0.25,
-    };
-
-    let creationStepsWithStatus = creationSteps.map(stepWithAction => {
-      const { creationStatus } = stepWithAction;
-
+    const creationStepsWithStatus = creationSteps.map(step => {
+      const wasDone = stepWasDone(step, accountInfo);
       // TODO: As soon as we store action status in redux, this logic will change
       // transaction pending, confirmed once, confirmed many times, failed, etc.
-      const status = creationActions[creationStatus] ? DONE : MISSING;
-
-      const creationStep = { ...stepWithAction, status };
-
-      return creationStep;
+      const status = wasDone ? DONE : MISSING;
+      return { ...step, status };
     });
-
-    creationStepsWithStatus = [accountStep, ...creationStepsWithStatus];
 
     return (
       <MyAccount
