8a05bef94a422be91bd64523c79d061f1e8a45f5
cmarcelo <cmarcelom@gmail.com>
2019-04-19 14:21:21 +0100

    Refactoring

--- decentraland/constants/AccountCreationActions.js
+++ decentraland/constants/AccountCreationActions.js
@@ -1,18 +1,22 @@
-import AccountCreationStatus from "./AccountCreationStatus";
+import {
+  FUNDING_WITH_ETH,
+  FUNDING_WITH_MANA,
+  APPROVING_MARKETPLACE,
+} from "./AccountCreationStatus";
 
 /**
  * Total percentage should be 80% because account generated is 20%
  */
 export default {
-  [AccountCreationStatus.FUNDING_WITH_ETH]: {
+  [FUNDING_WITH_ETH]: {
     name: "Funded with ETH",
     percentage: 0.25,
   },
-  [AccountCreationStatus.FUNDING_WITH_MANA]: {
+  [FUNDING_WITH_MANA]: {
     name: "Funded with MANA tokens",
     percentage: 0.25,
   },
-  [AccountCreationStatus.APPROVING_MARKETPLACE]: {
+  [APPROVING_MARKETPLACE]: {
     name: "Linked to marketplace",
     percentage: 0.25,
   },
