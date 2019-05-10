4b0a721c41087c0f1a79ec00dbc926f97595475c
Paul Cowgill <pauldavidcowgill@gmail.com>
2019-05-10 09:17:57 -0500

    Remove unused color var

--- decentraland/constants/Colors.js
+++ decentraland/constants/Colors.js
@@ -2,7 +2,6 @@ const WHITE = "white";
 const GRAY = "gray";
 const GRAY_LIGHT = "#CCCCCC";
 const GRAY_UNKNOWN = "#868686";
-const GRAY_DARK = "#2B2B2B";
 const BLACK = "black";
 const TEXT_COLOR = GRAY_LIGHT;
 const MAIN_GREEN = "#07FFC2";
