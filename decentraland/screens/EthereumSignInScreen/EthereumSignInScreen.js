5a956bd37fe5ab972732564972ef0fdcbc6c9ee7
Paul Cowgill <pauldavidcowgill@gmail.com>
2019-05-14 17:53:10 -0500

    Check if app is installed and open app store URL if not

--- decentraland/screens/EthereumSignInScreen/EthereumSignInScreen.js
+++ decentraland/screens/EthereumSignInScreen/EthereumSignInScreen.js
@@ -1,9 +1,26 @@
 import React from "react";
 import EthereumSignIn from "@presentational/EthereumSignIn";
+import AppLink from "react-native-app-link";
 
 export default class EthereumSignInScreen extends React.Component {
   render() {
-    const onConnect = () => {};
+    const onConnect = async () => {
+      try {
+        // TODO: Change config to use test wallet app
+        const config = {
+          appName: "lyft",
+          appStoreId: "529379082",
+          appStoreLocale: "us",
+          playStoreId: "me.lyft.android",
+        };
+        await AppLink.maybeOpenURL(url, config);
+      } catch {
+        err => {
+          // handle error
+          console.log("Error", error);
+        };
+      }
+    };
     return <EthereumSignIn onConnect={onConnect} />;
   }
 }
