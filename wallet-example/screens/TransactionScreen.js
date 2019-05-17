5b72e1ff4bb789255517c7283298cb55bb1b93a5
Paul Cowgill <pauldavidcowgill@gmail.com>
2019-05-17 10:33:57 -0500

    Showing latest tx with tx data

--- wallet-example/screens/TransactionScreen.js
+++ wallet-example/screens/TransactionScreen.js
@@ -11,15 +11,30 @@ export class TransactionScreen extends React.Component {
   render() {
     const { transactions } = this.props;
     let transactionsString = "placeholder";
-    try {
-      console.log("# of transactions", transactions.length);
-      transactionsString = JSON.stringify(transactions);
-    } catch (error) {
-      console.info("error", error);
-    }
+    console.log("# of transactions", transactions.length);
+    const latestTx = transactions[transactions.length - 1];
+    console.log("latestTx", latestTx);
+    const { to } = latestTx;
+    const { from } = latestTx;
+    const { amount } = latestTx;
+    // transactionsString = JSON.stringify(latestTx);
     return (
       <View style={styles.container}>
-        <Text>{transactionsString}</Text>
+        {!to ? (
+          <Text>{"Missing 'to' address"}</Text>
+        ) : (
+          <Text>{`to ${to}`}</Text>
+        )}
+        {!from ? (
+          <Text>{"Missing 'from' address"}</Text>
+        ) : (
+          <Text>{`from ${from}`}</Text>
+        )}
+        {!amount ? (
+          <Text>{"Missing 'amount'"}</Text>
+        ) : (
+          <Text>{`amount ${amount}`}</Text>
+        )}
       </View>
     );
   }
@@ -28,6 +43,8 @@ export class TransactionScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
     paddingTop: 15,
     backgroundColor: "#fff"
   }
